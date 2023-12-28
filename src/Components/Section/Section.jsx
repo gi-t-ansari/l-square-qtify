79ee274c8e67c0f014c366a3dcbb41a8bf039e1b
gi-t-ansari <tanzeelshamsh.in@gmail.com>
2023-12-28 10:42:14 +0530

    added new albums

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -6,15 +6,28 @@ import Card from "../Card/Card"
 
 function Section() {
     const [topAlbums, setTopAlbums] = useState([]);
-    const [toggle, setToggle] = useState(false)
+    const [newAlbums, setNewAlbums] = useState([]);
+    const [toggle, setToggle] = useState(false);
 
-    useEffect(() => {
+    const getTopAlbums = () => {
         axios.get("https://qtify-backend-labs.crio.do/albums/top")
         .then((res) => setTopAlbums(res.data))
         .catch((e) => console.log(e))
+    }
+
+    const getNewAlbums = () => {
+        axios.get("https://qtify-backend-labs.crio.do/albums/new")
+        .then((res) => setNewAlbums(res.data))
+        .catch((e) => console.log(e))
+    }
+
+    useEffect(() => {
+        getTopAlbums();
+        getNewAlbums()
     },[])
 
   return (
+    <>
     <section className={styles.section}>
         <header className={styles.header}>
             <h4>Top Albums</h4>
@@ -24,6 +37,16 @@ function Section() {
             {topAlbums.slice(0, 14).map((album) => <Card key={album.id} id={album.id} image={album.image} follows={album.follows} title={album.title} />)}
         </article>
     </section>
+    <section className={styles.section}>
+        <header className={styles.header}>
+            <h4>New Albums</h4>
+            <button className={styles.button} onClick={() => setToggle(!toggle)} >{toggle ? "Collapse" : "Show All"}</button>
+        </header>
+        <article className={styles.article}>
+            {newAlbums.slice(0, 14).map((album) => <Card key={album.id} id={album.id} image={album.image} follows={album.follows} title={album.title} />)}
+        </article>
+    </section>
+    </>
   )
 }
 
